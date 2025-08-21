1934bb541aa9ca9b5f5e5b3dbe7d06ac37d60bfa
Takayuki Sase <takayuki.sase@firebear.co.jp>
2025-08-21 10:44:33 +0900

    setBoldエラーを修正、箇条書きと番号付きリストの太字処理を簡素化、新しいGAS URLに更新

--- digital_check_sheet_webapp.js
+++ digital_check_sheet_webapp.js
@@ -321,21 +321,7 @@ function createPdfReport(scores, geminiComment) {
                 // 箇条書き内の太字処理
                 const regex = /\*\*(.*?)\*\*/g;
                 listText = listText.replace(regex, '$1');
-                const listItem = body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.BULLET);
-                // 太字部分を後から適用
-                const textElements = listItem.getText().split(regex);
-                if (textElements.length > 1) {
-                    // 太字部分を検出して適用
-                    const regex2 = /\*\*(.*?)\*\*/g;
-                    let match;
-                    while ((match = regex2.exec(trimmedLine.substring(2))) !== null) {
-                        const boldText = match[1];
-                        const textElement = listItem.findText(boldText);
-                        if (textElement) {
-                            textElement.setBold(true);
-                        }
-                    }
-                }
+                body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.BULLET);
             } else {
                 body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.BULLET);
             }
@@ -346,17 +332,7 @@ function createPdfReport(scores, geminiComment) {
                 // 番号付きリスト内の太字処理
                 const regex = /\*\*(.*?)\*\*/g;
                 listText = listText.replace(regex, '$1');
-                const listItem = body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.NUMBER);
-                // 太字部分を後から適用
-                const regex2 = /\*\*(.*?)\*\*/g;
-                let match;
-                while ((match = regex2.exec(trimmedLine.replace(/^\d+\.\s/, ''))) !== null) {
-                    const boldText = match[1];
-                    const textElement = listItem.findText(boldText);
-                    if (textElement) {
-                        textElement.setBold(true);
-                    }
-                }
+                body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.NUMBER);
             } else {
                 body.appendListItem(listText).setGlyphType(DocumentApp.GlyphType.NUMBER);
             }
--- script.js
+++ script.js
@@ -134,7 +134,7 @@ async function handleFormSubmit(e) {
     loading.classList.remove('hidden');
 
     // ここにGASのウェブアプリURLを設定
-            const GAS_URL = 'https://script.google.com/macros/s/AKfycbyXHEyk_Ocy0RXFgr0O0cs3sRSwxEwKcRJUBVAtOLJQytjIYf5tbizXsjP6s6WjQKBqNw/exec';
+            const GAS_URL = 'https://script.google.com/macros/s/AKfycbyUW-S7xS7Omka_r5sqfaZPP_Y6XLAVM3CAOuubh25_2P10DLvS0hzAMUUNEQ38PHYwEA/exec';
 
     try {
         const response = await fetch(GAS_URL, {
